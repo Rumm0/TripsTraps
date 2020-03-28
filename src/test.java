372a171348159c0a9093234151e3d67ddbcbff2f
Rumm0 <kaarelrummo@gmail.com>
2020-03-28 23:55:00 +0200

    Väikesed parandused

--- src/test.java
+++ src/test.java
@@ -1,9 +1,8 @@
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class main {
+public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Esimene mängija:");
